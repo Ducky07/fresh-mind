c1cea3dc8a17ed68163126516822dd4e02e01301
Jack Valley <jack.valley@outlook.com>
2024-03-27 18:48:45 +0200

    Style changes
    Fixed mobile styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,32 +6,39 @@ export function Footer() {
     { name: "Contact", href: "/" },
   ];
   return (
-    <footer class="flex justify-between bg-zinc-900 p-4">
+    <footer class="grid grid-flow-col justify-between bg-zinc-900 p-4">
       <a href="/" class="self-center">
         <img src="/logo.svg" class="h-24 w-24" alt="Jack's Logo" />
       </a>
-      <div class="grid grid-cols-1 sm:grid-cols-2">
-        <div class="grid grid-cols-2 items-center p-4 text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
+      <div class="flex flex-col sm:flex-row sm:justify-between">
+        <div class="grid grid-cols-2 items-center text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1 w-max">
           {links.map((link) => (
-            <a href={link.href} class="hover:text-orange-300 sm:px-6">
+            <a
+              href={link.href}
+              class="hover:text-orange-400 sm:px-4 px-4 py-2"
+            >
               {link.name}
             </a>
           ))}
         </div>
-        <div class="flex items-center justify-evenly py-2">
+        <div class="flex items-center sm:w-fit">
           <a
+            target="_blank"
             href="https://github.com/Ducky07?tab=overview&from=2021-12-01&to=2021-12-31"
-            class="px-2"
+            class="px-4 py-2 sm:px-4"
           >
             <img src="/github.svg" class="h-8 w-8" alt="Github" />
           </a>
-          <a href="mailto:juuso.laakso@edu.turkuamk.fi" class="px-2">
+          <a
+            href="mailto:juuso.laakso@edu.turkuamk.fi"
+            class="px-4 py-2 sm:px-4"
+          >
             <img src="/mail.svg" class="h-8 w-8" alt="Email" />
           </a>
         </div>
       </div>
       <div
-        class="flex w-min text-left sm:w-fit sm:text-center"
+        class="flex text-left sm:w-fit"
         aria-hidden="true"
       >
         <p class="my-auto text-xs text-gray-400" aria-hidden="true">
