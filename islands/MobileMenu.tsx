1a139d0a3cb0180e3deb86a566fa03ac26b3b707
Jack Valley <jack.valley@outlook.com>
2024-03-27 19:44:54 +0200

    Linting

--- islands/MobileMenu.tsx
+++ islands/MobileMenu.tsx
@@ -19,34 +19,30 @@ export function MobileMenu() {
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
         >
-          {mobileMenuOpen
-            ? (
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              >
-              </path>
-            )
-            : (
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              >
-              </path>
-            )}
+          {mobileMenuOpen ? (
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          ) : (
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          )}
         </svg>
       </button>
       {mobileMenuOpen && (
-        <nav class="fixed right-2 flex flex-col rounded-md bg-zinc-900 text-left text-white ring-1 ring-orange-400 shadow-lg">
+        <nav class="fixed right-2 flex flex-col rounded-md bg-zinc-900 text-left text-white shadow-lg ring-1 ring-orange-400">
           {links.map((link) => (
             <a
               key={link.name}
               href={link.url}
-              class="p-2 m-2 text-base font-bold hover:text-orange-400"
+              class="m-2 p-2 text-base font-bold hover:text-orange-400"
             >
               {link.name}
             </a>
