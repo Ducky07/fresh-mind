73f35eb6af435f982bb180bb0abc8923be0bb3b1
Jack Valley <jack.valley@outlook.com>
2024-03-27 17:53:15 +0200

    Upgraded navigation
    Created hamburger menu for navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,25 +1,35 @@
+import { MobileMenu } from "@/islands/MobileMenu.tsx";
+
+const links = [
+  { url: "/", name: "Home" },
+  { url: "/about", name: "About" },
+  { url: "/blog", name: "Blog" },
+  { url: "/contact", name: "Contact" },
+];
+
 export function Navigation() {
-  const links = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About" },
-    { href: "/blog", label: "Blog" },
-    { href: "/contact", label: "Contact" },
-  ];
   return (
-    <div class="grid grid-flow-col items-center bg-zinc-900 p-4 sm:justify-between">
-      <header class="flex flex-col items-center text-white sm:flex-row">
+    <header
+      class="grid grid-flow-col items-center justify-between bg-zinc-900 p-4 sm:justify-between"
+      f-client-nav={false}
+    >
+      <div class="flex flex-col items-center text-white sm:flex-row">
         <a href="/">
           <img src="/logo.svg" class="h-16 w-16" alt="Jack's Logo" />
         </a>
         <p class="font-bold sm:ml-4 sm:text-2xl">Jack's Blog</p>
-      </header>
-      <nav class="grid grid-cols-2 items-center text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
+      </div>
+      <nav
+        class="hidden grid-cols-2 items-center text-left text-base font-medium text-white sm:grid sm:grid-flow-col sm:grid-rows-1"
+        f-client-nav={true}
+      >
         {links.map((link) => (
-          <a class="py-2 hover:text-orange-300 sm:px-6" href={link.href}>
-            {link.label}
+          <a class="py-2 hover:text-orange-300 sm:px-6" href={link.url}>
+            {link.name}
           </a>
         ))}
       </nav>
-    </div>
+      <MobileMenu />
+    </header>
   );
 }
