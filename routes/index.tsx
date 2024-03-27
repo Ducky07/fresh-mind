5eec7aece6be45162741fd577f3dbc61df798531
Jack Valley <jack.valley@outlook.com>
2024-03-27 17:58:12 +0200

    Better loading times (#1)
    Added f-client support.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div class="mx-auto bg-zinc-800">
-      <main class="mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center">
+    <main class="grid bg-zinc-800 text-center">
+      <div class="mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center">
         <img
           class="my-6"
           src="/logo.svg"
@@ -9,13 +9,8 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold text-white">Welcome to Fresh</h1>
-        <p class="my-4 text-white">
-          Try updating this message in the
-          <code class="mx-2 text-white">./routes/index.tsx</code> file, and
-          refresh.
-        </p>
-      </main>
-    </div>
+        <h1 class="text-4xl font-bold text-white">Welcome to my Universe</h1>
+      </div>
+    </main>
   );
 }
