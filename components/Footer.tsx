a8358dcd76aae1e2c054b60afc3dfe2f5c88135b
Jack Valley <jack.valley@outlook.com>
2024-03-27 18:50:57 +0200

    Made footer more mobile friendly (#2)
    * Initial footer changes
    
    New social icons
    
    * Style changes
    
    Fixed mobile styling
    
    * Ran linter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,21 +6,35 @@ export function Footer() {
     { name: "Contact", href: "/" },
   ];
   return (
-    <footer class="flex justify-between bg-zinc-900 p-4">
+    <footer class="grid grid-flow-col justify-between bg-zinc-900 p-4">
       <a href="/" class="self-center">
         <img src="/logo.svg" class="h-24 w-24" alt="Jack's Logo" />
       </a>
-      <div class="grid grid-cols-2 items-center text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
-        {links.map((link) => (
-          <a href={link.href} class="hover:text-orange-300 sm:px-6">
-            {link.name}
+      <div class="flex flex-col sm:flex-row sm:justify-between">
+        <div class="grid w-max grid-cols-2 items-center text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
+          {links.map((link) => (
+            <a href={link.href} class="px-4 py-2 hover:text-orange-400 sm:px-4">
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <div class="flex items-center sm:w-fit">
+          <a
+            target="_blank"
+            href="https://github.com/Ducky07?tab=overview&from=2021-12-01&to=2021-12-31"
+            class="px-4 py-2 sm:px-4"
+          >
+            <img src="/github.svg" class="h-8 w-8" alt="Github" />
           </a>
-        ))}
+          <a
+            href="mailto:juuso.laakso@edu.turkuamk.fi"
+            class="px-4 py-2 sm:px-4"
+          >
+            <img src="/mail.svg" class="h-8 w-8" alt="Email" />
+          </a>
+        </div>
       </div>
-      <div
-        class="flex w-min text-left sm:w-fit sm:text-center"
-        aria-hidden="true"
-      >
+      <div class="flex text-left sm:w-fit" aria-hidden="true">
         <p class="my-auto text-xs text-gray-400" aria-hidden="true">
           Jack Valley © 2024
         </p>
