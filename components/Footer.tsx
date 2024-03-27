196c4af5a5d1ff13c2fd03ca3b5cfa3738da1262
Jack Valley <jack.valley@outlook.com>
2024-03-27 17:50:47 +0200

    Initial footer changes
    New social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,12 +10,25 @@ export function Footer() {
       <a href="/" class="self-center">
         <img src="/logo.svg" class="h-24 w-24" alt="Jack's Logo" />
       </a>
-      <div class="grid grid-cols-2 items-center text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
-        {links.map((link) => (
-          <a href={link.href} class="hover:text-orange-300 sm:px-6">
-            {link.name}
+      <div class="grid grid-cols-1 sm:grid-cols-2">
+        <div class="grid grid-cols-2 items-center p-4 text-left text-base font-medium text-white sm:grid-flow-col sm:grid-rows-1">
+          {links.map((link) => (
+            <a href={link.href} class="hover:text-orange-300 sm:px-6">
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <div class="flex items-center justify-evenly py-2">
+          <a
+            href="https://github.com/Ducky07?tab=overview&from=2021-12-01&to=2021-12-31"
+            class="px-2"
+          >
+            <img src="/github.svg" class="h-8 w-8" alt="Github" />
           </a>
-        ))}
+          <a href="mailto:juuso.laakso@edu.turkuamk.fi" class="px-2">
+            <img src="/mail.svg" class="h-8 w-8" alt="Email" />
+          </a>
+        </div>
       </div>
       <div
         class="flex w-min text-left sm:w-fit sm:text-center"
