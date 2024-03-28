36f126d8c2401de59798ba55bc64ccf24a6f6cbb
Jack Valley <jack.valley@outlook.com>
2024-03-28 21:51:26 +0200

    Updated 404 page (#5)
    * Slight style change
    
    Changed styles to be more like the main content on index.tsx
    
    * Update README.md
    
    * Update deploy.yml
    
    * Workflow ignore main PR.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main class="grid bg-zinc-800 text-center">
-      <div class="prose mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center sm:prose-lg">
+      <div class="prose prose-invert mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center sm:prose-lg">
         <img
           class="my-6"
           src="/logo.svg"
@@ -9,7 +9,7 @@ export default function Home() {
           height="128"
           alt="Jack's universe logo"
         />
-        <h1 class="font-semibold text-white">Welcome to my Universe</h1>
+        <h1 class="font-semibold">Welcome to my Universe</h1>
       </div>
     </main>
   );
