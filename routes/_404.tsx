f380d1b4664c32b9132c09d92e7ff71614c49783
Jack Valley <jack.valley@outlook.com>
2024-03-28 21:36:29 +0200

    Slight style change
    Changed styles to be more like the main content on index.tsx

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -4,27 +4,27 @@ export default function Error404() {
   return (
     <>
       <Head>
-        <title>404 - Page not found</title>
+        <title>Fresh Mind - 404</title>
       </Head>
-      <div class="mx-auto bg-zinc-800 text-white">
-        <main class="mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center">
+      <main class="grid bg-zinc-800 text-center">
+        <div class="prose prose-invert mx-auto flex h-screen max-w-screen-md flex-col items-center justify-center sm:prose-lg">
           <img
             class="my-6"
             src="/logo.svg"
             width="128"
             height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
+            alt="Jack's universe logo"
           />
-          <h1 class="text-4xl font-bold">Page not found</h1>
+          <h1 class="mb-0">Page not found</h1>
           <p class="my-4">The page you were looking for doesn't exist.</p>
           <a
             href="/"
-            class="rounded-md bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+            class="rounded-md bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-black no-underline shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
           >
             Go back home
           </a>
-        </main>
-      </div>
+        </div>
+      </main>
     </>
   );
 }
